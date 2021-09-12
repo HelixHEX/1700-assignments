02829eaef181fbe933d0d8d0ba420ffc3bbaaef3
Elias <e.wambugu192@gmail.com>
2021-09-11 19:32:09 -0700

    first commit

---
+++ madlib.py
@@ -0,0 +1,81 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+verb1 = print("Enter a verb: ")
+
+print("")
+place = print("Now give me a place: ")
+
+print("")
+trans = print("Enter a mode of transportation: ")
+
+print("")
+adj1 = print("Enter an adjective: ")
+
+print("")
+verb2 = print("Enter another verb: ")
+
+print("")
+noun1 = print("Enter a noun: ")
+
+print("")
+noun2 = print("Now Give me another noun: ")
+
+print("")
+name1 = print("Enter a name: ")
+
+print("")
+verb3 = print("Now enter another verb: ")
+
+print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------
+# Partial solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = input("Name:")
+# color = input("color:")
+# num = input("Number:")
+
+# print(f"{name} wore {color} shoes while they counted to {num}")
